550cc6a5884a990fc1ed04f88f72e68cb3fe0139
farisbegic <faris.begic@stu.ssst.edu.ba>
2022-04-12 09:08:29 +0200

    Implemented Getter and Setter using Lombok and created getToyById method

--- src/main/java/com/toyvalley/controllers/ToyController.java
+++ src/main/java/com/toyvalley/controllers/ToyController.java
@@ -3,6 +3,7 @@ package com.toyvalley.controllers;
 import com.toyvalley.models.Toy;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
@@ -20,5 +21,15 @@ public class ToyController {
     @GetMapping
     public List<Toy> getToy() {
         return this.toyList;
+    }
+
+    @GetMapping("{/id}")
+    public Toy getToy(@PathVariable long id) {
+        for (Toy toy : toyList) {
+            if (toy.getId() == id) {
+                return toy;
+            }
+        }
+        return null;
     }
 }
--- src/main/java/com/toyvalley/models/Toy.java
+++ src/main/java/com/toyvalley/models/Toy.java
@@ -1,5 +1,10 @@
 package com.toyvalley.models;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Toy {
     long id;
     String brand;
