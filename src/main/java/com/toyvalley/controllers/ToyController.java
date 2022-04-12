1fc853e9fd917f7952b6c4972f127c41e29a4ba2
farisbegic <faris.begic@stu.ssst.edu.ba>
2022-04-12 09:05:47 +0200

    Created controller, models and getAllToys mapping

---
+++ src/main/java/com/toyvalley/controllers/ToyController.java
@@ -0,0 +1,24 @@
+package com.toyvalley.controllers;
+
+import com.toyvalley.models.Toy;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("/toys")
+public class ToyController {
+    private ArrayList<Toy> toyList;
+
+    public ToyController() {
+        this.toyList = new ArrayList<>();
+    }
+
+    @GetMapping
+    public List<Toy> getToy() {
+        return this.toyList;
+    }
+}
---
+++ src/main/java/com/toyvalley/models/Toy.java
@@ -0,0 +1,8 @@
+package com.toyvalley.models;
+
+public class Toy {
+    long id;
+    String brand;
+    String model;
+    Condition condition;
+}
