b45439980293cff48a71e950b711b42cc538db23
farisbegic <faris.begic@stu.ssst.edu.ba>
2022-04-12 08:39:32 +0200

    Initial commit

---
+++ src/main/java/com/toyvalley/ToyValleyApplication.java
@@ -0,0 +1,13 @@
+package com.toyvalley;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyValleyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyValleyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toyvalley/ToyValleyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toyvalley;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyValleyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
