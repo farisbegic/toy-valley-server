d8c599d15d568ffe6baf08f0c626aed18fdda644
farisbegic <faris.begic@stu.ssst.edu.ba>
2022-05-09 09:11:17 +0200

    Added environment variable for frontend url and changed in cors config & added db name environment variable

--- src/main/java/com/toyvalley/config/CorsConfiguration.java
+++ src/main/java/com/toyvalley/config/CorsConfiguration.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfiguration {
-    @Value("http://localhost:4200")
+    @Value("${service.ui.url}")
     private String[] allowedOrigins;
 
     @Bean
