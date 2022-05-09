568f1e4ed253332d4f73652e57d1b975c8d4bb78
Faris Begić <73107935+farisbegic@users.noreply.github.com>
2022-05-09 09:49:54 +0200

    70 backend deployment (#9)
    * Added environment variable for frontend url and changed in cors config & added db name environment variable
    
    * Optimized controller imports
    
    * Added system properties file

--- src/main/java/com/toyvalley/controllers/CityController.java
+++ src/main/java/com/toyvalley/controllers/CityController.java
@@ -1,12 +1,13 @@
 package com.toyvalley.controllers;
 
 import com.toyvalley.models.data.city.CityResponse;
-import com.toyvalley.models.entities.City;
-import com.toyvalley.models.entities.User;
 import com.toyvalley.services.CityService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
