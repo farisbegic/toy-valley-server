7a43785f23ce4388608f3ad29183b9a1d0d9c348
farisbegic <faris.begic@stu.ssst.edu.ba>
2022-05-21 08:10:03 +0200

    Implemented unit test for getToys method

--- src/test/java/com/toyvalley/services/ToyServiceUnitTest.java
+++ src/test/java/com/toyvalley/services/ToyServiceUnitTest.java
@@ -1,21 +1,32 @@
 package com.toyvalley.services;
 
+import com.toyvalley.data.ToyTest;
+import com.toyvalley.models.data.toy.ToyResponse;
+import com.toyvalley.models.entities.Toy;
 import com.toyvalley.repositories.ToyRepository;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
 public class ToyServiceUnitTest {
 
     @MockBean
     private ToyRepository toyRepository;
 
     @TestConfiguration
-    static class ItemServiceTestContextConfiguration {
-
+    static class ToyServiceTestContextConfiguration {
         @Bean
         @Primary
         public ToyService toyService(ToyRepository toyRepository) {
@@ -26,5 +37,19 @@ public class ToyServiceUnitTest {
     @Autowired
     private ToyService toyService;
 
+    @Test
+    public void givenToys_whenGetToys_thenListShouldBeFound() {
+        ArrayList<Toy> toyResponse = new ArrayList<>();
+        toyResponse.add(ToyTest.toy());
+        Mockito.when(toyRepository.findAll()).thenReturn(toyResponse);
 
+        List<ToyResponse> returnedItems = toyService.getToy();
+
+        assertThat(returnedItems).hasSize(1);
+    }
+
+    @Test
+    public void givenNoToys_whenGetToys_thenListShouldBeEmpty() {
+        assertThat(toyService.getToy()).isEmpty();
+    }
 }
