114d70e15e79ad4c568849058bdde4625a5452b1
azrakurtic <azrakurtic@gmail.com>
2022-05-25 08:47:56 +0200

    Added tests for all user service methods

---
+++ src/test/java/com/toyvalley/data/UserTest.java
@@ -0,0 +1,37 @@
+package com.toyvalley.data;
+
+import com.toyvalley.models.data.toy.CreateToyRequest;
+import com.toyvalley.models.data.user.CreateUserRequest;
+import com.toyvalley.models.data.user.UpdateUserRequest;
+import com.toyvalley.models.data.user.UserResponse;
+import com.toyvalley.models.entities.City;
+import com.toyvalley.models.entities.Toy;
+import com.toyvalley.models.entities.User;
+
+public class UserTest {
+
+  public static User user() {
+    User user = new User();
+    user.setId(1L);
+    user.setName("Azra");
+    user.setSurname("Kurtic");
+    user.setPhone("123456");
+    user.setAddress("Address 1");
+    City city = new City();
+    city.setId(1L);
+    city.setName("Sarajevo");
+    user.setCity(city);
+    user.setEmail("azra@gmail.com");
+    user.setPassword("Pass123456");
+    return user;
+  }
+
+  public static CreateUserRequest createUserRequest(User inputUser) {
+    return new CreateUserRequest(inputUser.getName(), inputUser.getSurname(), inputUser.getPhone(), inputUser.getAddress(), inputUser.getCity(), inputUser.getEmail(), inputUser.getPassword());
+  }
+
+  public static UpdateUserRequest updateUserRequest(User inputUser) {
+    return new UpdateUserRequest(inputUser.getName(), inputUser.getSurname(), inputUser.getPhone(), inputUser.getAddress(), inputUser.getEmail(), inputUser.getPassword(), inputUser.getCity(), inputUser.isActive());
+  }
+
+}
--- src/test/java/com/toyvalley/services/UserServiceUnitTest.java
+++ src/test/java/com/toyvalley/services/UserServiceUnitTest.java
@@ -1,10 +1,16 @@
 package com.toyvalley.services;
 
+import com.toyvalley.data.ToyTest;
+import com.toyvalley.models.data.toy.CreateToyRequest;
+import com.toyvalley.models.data.toy.ToyResponse;
+import com.toyvalley.models.data.toy.UpdateToyRequest;
 import com.toyvalley.models.data.user.CreateUserRequest;
+import com.toyvalley.models.data.user.UpdateUserRequest;
 import com.toyvalley.models.data.user.UserResponse;
+import com.toyvalley.models.entities.Toy;
 import com.toyvalley.models.entities.User;
 import com.toyvalley.repositories.UserRepository;
-import data.UserTest;
+import com.toyvalley.data.UserTest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
@@ -20,6 +26,7 @@ import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
@@ -71,19 +78,36 @@ public class UserServiceUnitTest {
       .hasMessageContaining("User with id " + 32 + " is not found");
   }
 
-  /*@Test
-  public void givenUser_whenCreate_thenIdAssigned() {
-    CreateUserRequest inputUser = UserTest.createUserRequest();
-
+  @Test
+  public void givenUser_whenCreate_thenUserReturned() {
+    User inputUser = UserTest.user();
     User outputUser = UserTest.user();
+    Mockito.when(userRepository.save(any(User.class))).thenReturn(outputUser);
 
-    Mockito.when(userRepository.save(outputUser))
-      .thenReturn(outputUser);
+    CreateUserRequest requestUser = UserTest.createUserRequest(inputUser);
+    UserResponse returnedUser = userService.createUser(requestUser);
 
-    UserResponse resultUser = userService.createUser(inputUser);
+    assertThat(returnedUser).isNotNull();
+    assertThat(returnedUser.getName()).isEqualTo(inputUser.getName());
+  }
 
-    assertThat(resultUser.getId()).isNotEqualTo(0L);
-  }*/
+  @Test
+  public void givenItemAndValidId_whenUpdate_thenItemReturned() {
+    User inputUser = UserTest.user();
+    inputUser.setId(0L);
+    User outputUser = UserTest.user();
+    long id = 1L;
+
+    Mockito.when(userRepository.findById(id)).thenReturn(Optional.of(outputUser));
+    Mockito.when(userRepository.save(inputUser)).thenReturn(outputUser);
+
+    UpdateUserRequest updateUser = UserTest.updateUserRequest(inputUser);
+    UserResponse returnedUser = userService.updateUser(id, updateUser);
+
+    assertThat(returnedUser).isNotNull();
+    assertThat(returnedUser.getName()).isEqualTo(inputUser.getName());
+    assertThat(returnedUser.getId()).isEqualTo(id);
+  }
 
   @Test
   public void givenUser_whenDelete_thenRepositoryCalled() {
