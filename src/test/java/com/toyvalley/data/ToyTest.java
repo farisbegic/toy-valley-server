62fac8b58b0cebf7293347ae251a0ae4eea51090
farisbegic <faris.begic@stu.ssst.edu.ba>
2022-05-25 04:01:25 +0200

    Fixed create service test issues and made mock data constructor private

--- src/test/java/com/toyvalley/data/ToyTest.java
+++ src/test/java/com/toyvalley/data/ToyTest.java
@@ -3,9 +3,12 @@ package com.toyvalley.data;
 import com.toyvalley.models.entities.Toy;
 import com.toyvalley.models.enums.Condition;
 import com.toyvalley.models.enums.Gender;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ToyTest {
     public static Toy toy() {
         return new Toy(1L, "Star Wars", "A great toy", "Lego", Gender.unisex, Condition.brandNew, 0, new Date());
