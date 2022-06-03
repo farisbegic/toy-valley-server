c116e810d1ceada9c1307a4746b1ede317e5b5b4
Adna Salković <74348381+adnasal@users.noreply.github.com>
2022-06-03 23:22:51 +0200

    Removed warnings.

--- src/main/java/com/toyvalley/util/JwtUtil.java
+++ src/main/java/com/toyvalley/util/JwtUtil.java
@@ -31,7 +31,7 @@ public class JwtUtil {
 
     private Claims extractAllClaims(String token) {
         return Jwts.parser().setSigningKey(secretKey)
-                .parseClaimsJws(token) // throws: ExpiredJwtException among else
+                .parseClaimsJws(token)
                 .getBody();
     }
 
@@ -43,7 +43,7 @@ public class JwtUtil {
     private String createToken(Map<String, Object> claims, String subject) {
         return Jwts.builder().setClaims(claims).setSubject(subject)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * tokenDuration))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000L * 60 * tokenDuration))
                 .signWith(SignatureAlgorithm.HS256, secretKey).compact();
     }
 }
